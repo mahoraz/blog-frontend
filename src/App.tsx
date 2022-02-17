fbca25d666f51516e1c99672428c50b869031d66
Jaanus <jaanus@stagnationlab.com>
2022-02-17 13:33:21 +0200

    routing and first API fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom';
+import ProfileView from './views/users/profileView';
+import Index from './views';
+
+
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/users">About</Link>
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
+        <Routes>
+          <Route path="/" element={Index()}/>
+          <Route path="/users" element={ProfileView()}/>
+        </Routes>
       </header>
     </div>
   );
